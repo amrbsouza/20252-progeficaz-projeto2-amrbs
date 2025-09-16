f8fc50201e30fc8d30c8997f06868d0582a630ef
Alvaro Souza <alvaromodestorbsouza@gmail.com>
2025-09-16 10:47:27 -0300

    feat: implementa conexão com o banco de dados hospeado na plataforma Aiven

--- utils.py
+++ utils.py
@@ -1,5 +1,31 @@
+import mysql.connector
+import os
+from mysql.connector import Error
+from dotenv import load_dotenv
+
+load_dotenv('.cred')
+
+# Configurações para conexão com o banco de dados usando variáveis de ambiente
+config = {
+    'host': os.getenv('DB_HOST', 'localhost'),  # Obtém o host do banco de dados da variável de ambiente
+    'user': os.getenv('DB_USER'),  # Obtém o usuário do banco de dados da variável de ambiente
+    'password': os.getenv('DB_PASSWORD'),  # Obtém a senha do banco de dados da variável de ambiente
+    'database': os.getenv('DB_NAME'),  # Obtém o nome do banco de dados da variável de ambiente
+    'port': int(os.getenv('DB_PORT', 3306)),  # Obtém a porta do banco de dados da variável de ambiente
+    'ssl_ca': os.getenv('SSL_CA_PATH')  # Caminho para o certificado SSL
+}
+
 def connect_db():
-    pass
+    """Estabelece a conexão com o banco de dados usando as configurações fornecidas."""
+    try:
+        # Tenta estabelecer a conexão com o banco de dados usando mysql-connector-python
+        conn = mysql.connector.connect(**config)
+        if conn.is_connected():
+            return conn
+    except Error as err:
+        # Em caso de erro, imprime a mensagem de erro
+        print(f"Erro: {err}")
+        return None
 
 def get_imoveis(cidade=None, tipo=None):
     conn = connect_db()
